cf4dc02075c99c17923d0a4b9cf98c7f5b5369ef
pawoods <>
2023-05-09 16:43:40 +0000

    fixes bugs with commas rendering at the top of the table

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function getData(url, cb) {
 function getTableHeaders(obj) {
     var tableHeaders = [];
 
-    Object.keys(obj).forEach(function(key) {
+    Object.keys(obj).forEach(function (key) {
         tableHeaders.push(`<td>${key}</td>`);
     });
 
@@ -23,13 +23,13 @@ function getTableHeaders(obj) {
 }
 
 function generatePaginationButtons(next, prev) {
-    if(next && prev) {
+    if (next && prev) {
         return `<button onclick="writeToDocument('${prev}')">Previous</button>
-                <button onclick="writeToDocument('${next}')">Next</button>`
+                <button onclick="writeToDocument('${next}')">Next</button>`;
     } else if (next && !prev) {
-        return `<button onclick="writeToDocument('${next}')">Next</button>`
+        return `<button onclick="writeToDocument('${next}')">Next</button>`;
     } else if (!next && prev) {
-        return `<button onclick="writeToDocument('${prev}')">Previous</button>`
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
     }
 };
 
@@ -38,25 +38,25 @@ function writeToDocument(url) {
     var el = document.getElementById("data");
     el.innerHTML = "";
 
-    getData(url, function(data) {
+    getData(url, function (data) {
         var pagination;
         if (data.next || data.previous) {
-            pagination = generatePaginationButtons(data.next, data.previous)
+            pagination = generatePaginationButtons(data.next, data.previous);
         }
         data = data.results;
         var tableHeaders = getTableHeaders(data[0]);
 
-        data.forEach(function(item) {
+        data.forEach(function (item) {
             var dataRow = [];
 
-            Object.keys(item).forEach(function(key) {
+            Object.keys(item).forEach(function (key) {
                 var rowData = item[key].toString();
                 var truncatedData = rowData.substring(0, 15);
-                dataRow.push(`<td>${truncatedData}</td>`)
+                dataRow.push(`<td>${truncatedData}</td>`);
             });
             tableRows.push(`<tr>${dataRow}</tr>`);
         });
 
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g, "");
     });
 }
